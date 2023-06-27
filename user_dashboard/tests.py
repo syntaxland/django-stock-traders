c8b1c771e33c76f279a89ac3beb8d01c25902a88
syntaxland <chibuzo.okenwa@email.com>
2023-06-27 11:40:03 +0100

    new update

--- user_dashboard/tests.py
+++ user_dashboard/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 class UserDashboardTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
+    # def setUp(self):
+    #     self.client = Client()
 
     def test_dashboard_view(self):
         url = reverse('dashboard')
